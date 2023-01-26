d16212c62481db4c37c9fd1767b3dca58bf4f58f
Zobayer Hossain <90368480+Zobayer6@users.noreply.github.com>
2023-01-26 19:07:25 +0600

    variablet

---
+++ variable.js
@@ -0,0 +1,15 @@
+//numeric
+
+var price = 49;
+var age = 50;
+var weight = 70;
+
+//string
+var name = "abdul kuddus";
+var location = "panchbibi";
+
+//boolean
+
+var ismarried = true;
+var serious = true;
+var issingle = false;
